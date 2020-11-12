db00e17f008cfb13723cda20eb4ff03abaaebfce
Ruslan Kotaev <60106948+rkotaev@users.noreply.github.com>
2020-11-12 14:26:45 +0300

    initial commit
    Firebase added

---
+++ Message/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Message
+//
+//  Created by Project K Developer on 11.11.2020.
+//
+
+import SwiftUI
+import Firebase
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
